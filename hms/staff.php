cf578f6068e91c5baf79be830b650d582c4a7066
Peter Pine <petermwangipro@gmail.com>
2019-10-19 10:42:32 +0300

    Update staff.php

--- hms/staff.php
+++ hms/staff.php
@@ -58,6 +58,9 @@
                         </div>
                     </div>
                     <div class="card-body">
+                        
+                        //Creating a form which will hold all the staff details when input is placed
+                        
                         <form class="form-group" action="func.php" method="post">
                             <label>First Name: </label>
                             <input type="text" name="fname" class="form-control"><br>
